04615b8fbe447e50db973eb689ef8fa54ee9e7af
allan lenny <owinoallan77@gmail.com>
2023-05-25 17:55:33 +0300

    header

--- allashe-portfolio/src/Components/Header.jsx
+++ allashe-portfolio/src/Components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import image2 from '../images/image2.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Allan Lenny</h1>
+        <h5 className="text-light">
+          Fullstack Developer
+        </h5>
+        <CTA />
+        <div className="me">
+          <img src={image2} alt="profile" />
+        </div>
+      </div>
+    </header>
   )
 }
 
