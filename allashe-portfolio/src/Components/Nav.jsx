0978ac505359360614f02a6386c54eeb87485177
allan lenny <owinoallan77@gmail.com>
2023-05-26 04:05:40 +0300

    tooltips

--- allashe-portfolio/src/Components/Nav.jsx
+++ allashe-portfolio/src/Components/Nav.jsx
@@ -11,15 +11,27 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} ><AiTwotoneHome style={{ height: '30px', width: '30px', color: '#00001b' }} /></a>
+      <div className="links">
+        <a id='l1' href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} ><AiTwotoneHome style={{ height: '30px', width: '30px', color: '#00001b' }} />
+          <small className='tooltip1'>Home</small>
+        </a>
       
-      <a href="#About" onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><FaUserTie style={{ height: '30px', width: '30px', color: '#00001b' }} /></a>
+        <a id='l2' href="#About" onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><FaUserTie style={{ height: '30px', width: '30px', color: '#00001b' }} />
+        <small className="tooltip2">about</small>
+        </a>
       
-      <a href="#Experience" onClick={() => setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}><SiMdbook style={{ height: '30px', width: '30px', color: '#00001b' }} /></a>
+        <a id='l3' href="#Experience" onClick={() => setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}><SiMdbook style={{ height: '30px', width: '30px', color: '#00001b' }} />
+        <small className="tooltip3">experience</small>
+        </a>
       
-      <a href="#Services" onClick={() => setActiveNav('#Services')} className={activeNav === '#Services' ? 'active' : ''}><RiServiceFill style={{ height: '30px', width: '30px', color: '#00001b' }} /></a>
+        <a id='l4' href="#Services" onClick={() => setActiveNav('#Services')} className={activeNav === '#Services' ? 'active' : ''}><RiServiceFill style={{ height: '30px', width: '30px', color: '#00001b' }} />
+        <small className="tooltip4">services</small>
+        </a>
       
-      <a href="#Contact" onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><AiFillContacts style={{height:'30px', width:'30px', color:'#00001b'}}/></a>
+        <a id='l5' href="#Contact" onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><AiFillContacts style={{ height: '30px', width: '30px', color: '#00001b' }} />
+          <small className="tooltip5">contact</small>
+        </a>
+        </div>
       </nav>
   )
 }
