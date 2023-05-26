cba77fe29473df59f3534e170b7e0e7837e19115
allan lenny <owinoallan77@gmail.com>
2023-05-26 16:49:09 +0300

    testimonials avatar

--- allashe-portfolio/src/Components/Portfolio.jsx
+++ allashe-portfolio/src/Components/Portfolio.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './Portfolio.css'
+import image1 from '../images/image1.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>Recent Work</h5>
+      <h1>Portfolio</h1>
+
+      <div className="portfolio-container">
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>the coast</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>simple web</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>blogs</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>snake game</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>solar system</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="port-item-img"><img src={image1} alt="" /></div>
+          <h3>E-Commerce</h3>
+          <a href="https://github.com" className='btn' target='blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- allashe-portfolio/src/Components/Testimonials.jsx
+++ allashe-portfolio/src/Components/Testimonials.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 import './Testimonials.css'
+import avatar from '../images/image2.jpg'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+const data = [
+  {
+    avatar: avatar,
+    name: 'Vince',
+    review: 'Did a wonderful project'
+  },
+  {
+    avatar: avatar,
+    name: 'John',
+    review: 'Did a wonderful job'
+  },
+  {
+    avatar: avatar,
+    name: 'Henry',
+    review: 'Works Well'
+  },
+  {
+    avatar: avatar,
+    name: 'Tom',
+    review: 'Highly Recommend'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='Testimonials' >Testimonials</section>
+    <section id='Testimonials' >
+      <h5>Review</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          color: '#fff',
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper">
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className="tests">
+                <div className="avatar">
+                  <img src={avatar} alt="" />
+                <h5 className="client-name">{name}</h5>
+                <small className="client-review">{review}</small>
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
