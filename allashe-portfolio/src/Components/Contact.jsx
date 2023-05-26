c0b33294876df519b43aa52daf84d0c453252fef
allan lenny <owinoallan77@gmail.com>
2023-05-26 17:37:59 +0300

    contact

--- allashe-portfolio/src/Components/Contact.jsx
+++ allashe-portfolio/src/Components/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import './Contact.css';
+import {MdEmail} from 'react-icons/md'
+import {BsMessenger} from 'react-icons/bs'
+import {IoLogoWhatsapp} from 'react-icons/io'
 
 const Contact = () => {
   return (
-    <section id='Contact'>Contact</section>
+    <section id='Contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="contact-container">
+        <div className="contact-options">
+          <article className="option">
+            <MdEmail/>
+            <h4>Email</h4>
+            <h5>owinoallan77@gmail.com</h5>
+            <a href="mailto:owinoallan77@gmail.com" target='blank'>Send a message</a>
+          </article>
+          <article className="option">
+            <BsMessenger/>
+            <h4>Messenger</h4>
+            <h5>Allan Lenny</h5>
+            <a href="https://m.me/allano.allashe" target='blank'>Send a message</a>
+          </article>
+          <article className="option">
+            <IoLogoWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>+254703817561</h5>
+            <a href="https://api.whatsapp.com/send?phone+254703817561" target='blank'> Send a message</a>
+          </article>
+        </div>
+        <form action="#">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" id="message" cols="21" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
